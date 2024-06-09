42ba2784c1a244063ee23c03b844c95fd29b31b2
Hylan Silva <hylansilva@outlook.com>
2024-06-09 12:02:59 -0300

    atualizando sistema

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,17 @@ class Settings(BaseSettings):
     )
 
     SECRET_DATABASE_PATH: str
+    type: str
+    project_id: str
+    private_key_id: str
+    private_key: str
+    client_email: str
+    client_id: str
+    auth_uri: str
+    token_uri: str
+    auth_provider_x509_cert_url: str
+    client_x509_cert_url: str
+    universe_domain: str
 
 
 settings = Settings()
--- app/core/database.py
+++ app/core/database.py
@@ -4,12 +4,27 @@ from firebase_admin import credentials, firestore, initialize_app
 
 from app.core.config import settings
 
+
+doc_cred = {
+    "type": f"{settings.type}",
+    "project_id": f"{settings.project_id}",
+    "private_key_id": f"{settings.private_key_id}",
+    "private_key": f"{settings.private_key}",
+    "client_email": f"{settings.client_email}",
+    "client_id": f"{settings.client_id}",
+    "auth_uri": f"{settings.auth_uri}",
+    "token_uri": f"{settings.token_uri}",
+    "auth_provider_x509_cert_url": f"{settings.auth_provider_x509_cert_url}",
+    "client_x509_cert_url": f"{settings.auth_provider_x509_cert_url}",
+    "universe_domain": f"{settings.universe_domain}"
+}
+
 try:
-    cred = credentials.Certificate(settings.SECRET_DATABASE_PATH)
+    cred = credentials.Certificate(doc_cred)
     initialize_app(cred)
 except firebase_admin.exceptions.FirebaseError as e:
     raise HTTPException(
-        status_code=500, detail=f'Firebase initialization error: {e}'
+        status_code=500, detail=f"Firebase initialization error: {e}"
     )
 
 database = firestore.client()
